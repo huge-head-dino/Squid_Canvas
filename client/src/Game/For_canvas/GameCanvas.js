c3e89add9f4a5e7c9a640b04fd71c615594ba75a
ywkim727 <114053340+ywkim727@users.noreply.github.com>
2023-08-04 15:45:59 +0900

    fix: 제시어 위치 수정

--- client/src/Game/For_canvas/GameCanvas.js
+++ client/src/Game/For_canvas/GameCanvas.js
@@ -2,9 +2,9 @@ import React, { useContext } from "react";
 import RealCanvas from './RealCanvas'
 //JUNHO: 참고 import만 남김, 나중에 삭제 예정
 
-import { ClearCanvasButton } from './painter_tool';
-import { ColorPicker } from './painter_tool';
-import { LineWidthButtons } from './painter_tool';
+// import { ClearCanvasButton } from './painter_tool';
+// import { ColorPicker } from './painter_tool';
+// import { LineWidthButtons } from './painter_tool';
 
 // YEONGWOO: context 추가
 import SessionContext from '../../Openvidu/SessionContext'
@@ -24,10 +24,10 @@ function GameCanvas() {
       <div className="RealCanvas_3">
         <div className='RealCanvas_2' style={{ height: "100%"}}>
           <RealCanvas mySessionId = { mySessionId } myUserName = {myUserName}/>
+          <h1 style={{ fontWeight: "bold" }}>사과</h1>
         </div>
         <div className='ButtonZone'>
           <div style={{ position: "absolute", marginBottom: 'auto', color: "gray", fontSize: "24px", zIndex: 100 }}>
-          <h1 style={{ fontWeight: "bold" }}>사과</h1>
           {/* MRSEO: 조건 추가 */}
 
           {/* <div>
