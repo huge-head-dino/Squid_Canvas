567251a16d437eb0da8e76637fc504fc99a8dd95
huge-head-dino <sjunho98@gamil.com>
2023-08-09 13:40:54 +0900

    feat: 대기실 설명 사진과 함께 추가

--- client/src/Game/BasicUI.js
+++ client/src/Game/BasicUI.js
@@ -4,7 +4,12 @@ import useStore from "../store";
 import "./BasicUI.css";
 
 // SANGYOON: BGM 설정
-import { WaitingSound, CompetitionSound, SpySound, GameEntranceEffect } from "./audio";
+import {
+  WaitingSound,
+  CompetitionSound,
+  SpySound,
+  GameEntranceEffect,
+} from "./audio";
 
 // JANG: 경쟁, 스파이 모드 컴포넌트 분리
 import CompetitionUI from "./CompetitionUI";
@@ -58,7 +63,6 @@ function BasicUI() {
     sortGamer();
   }, [gamers]);
 
-
   // JUNHO: 1
   const { mode, setMode } = useStore();
 
@@ -84,25 +88,24 @@ function BasicUI() {
 
   // SANGYOON: 대기실에서 사용하는 sound effect
   const clickCompetition = () => {
-    WaitingSound.pause();             // 오징어격투 클릭시 WaitingSound play off
+    WaitingSound.pause(); // 오징어격투 클릭시 WaitingSound play off
     GameEntranceEffect.play();
-    CompetitionSound.loop = true;     // 반복재생
+    CompetitionSound.loop = true; // 반복재생
     CompetitionSound.currentTime = 0; // 곡 처음부터 시작
-    CompetitionSound.play();          // 오징어격투 클릭시 CompetitionSound play on
+    CompetitionSound.play(); // 오징어격투 클릭시 CompetitionSound play on
     CompetitionSound.volume = 0.5;
     setMode("competition");
   };
 
   const clickSpy = () => {
-    WaitingSound.pause();     // 스파이문어 클릭시 WaitingSound play off
+    WaitingSound.pause(); // 스파이문어 클릭시 WaitingSound play off
     GameEntranceEffect.play();
     SpySound.loop = true;
     SpySound.currentTime = 0;
-    SpySound.play();          // 스파이문어 클릭시 SpySound play on
+    SpySound.play(); // 스파이문어 클릭시 SpySound play on
     SpySound.volume = 0.5;
     setMode("spy");
   };
-  
 
   // JANG: 08.06 - 모드 선택 애니메이션
   const bounce = keyframes`
@@ -142,7 +145,7 @@ function BasicUI() {
               >
                 <Box
                   className="Game_Character"
-                  h="100%"
+                  h="90%"
                   w="15%"
                   bg="rgba(255, 255, 255, 0.7)"
                   backdropFilter="auto" // 블러
@@ -181,28 +184,26 @@ function BasicUI() {
 
                     {/* JUNHO: Join Button */}
 
-                    
-                      <Button
-                        colorScheme="teal"
-                        flex={1}
-                        size="xl"
-                        fontSize="50px" // 글자 크기 조정
-                        onClick={joinSelectedMode}
-                        className="Button_Mode"
-                        _hover={{ transform: "scale(1.1)" }}
-                      >
-                        게임룸 입장
-                      </Button>
-                   
+                    <Button
+                      colorScheme="teal"
+                      flex={1}
+                      size="xl"
+                      fontSize="50px" // 글자 크기 조정
+                      onClick={joinSelectedMode}
+                      className="Button_Mode"
+                      _hover={{ transform: "scale(1.1)" }}
+                    >
+                      게임룸 입장
+                    </Button>
                   </Grid>
                 </Box>
                 <Box
-                  h="100%"
+                  h="90%"
                   bg="rgba(255, 255, 255, 0.7)"
                   backdropFilter="auto"
                   backdropBlur="5px"
                   boxShadow="rgba(0, 0, 0, 0.2) 0px 1px 8px"
-                  w="50%"
+                  w="70%"
                   display="flex"
                   alignItems="center"
                   justifyContent="center"
@@ -226,26 +227,66 @@ function BasicUI() {
                   >
                     {/* Mode Descriptions */}
                     {selectedMode === "competition" && (
-                      <div>이 모드는 오징어 격투 입니다.
-                      1. 2 대 2 로 팀을 나누어 진행합니다.
-                      2. 팀 안에 그리는 사람, 맞추는 사람의 역할이 나누어 지며 정답을 맞추면 역할이 서로 바뀌게 됩니다.
-                      3. 레드팀, 블루팀 총 2라운드로 진행되며 각 라운드당 제한시간 70초 내에 더 많은 답을 맞춘 팀이 승리합니다.
-                      4. 라운드가 진행되는 동안, 상대팀은 '방해하기' 아이템을 한 번 사용할 수 있으며 10초 동안 캔버스에 끼어들어 같이 그림을 그릴 수 있습니다. '방해하기' 아이템을 통해 상대팀을 최대한 헷갈리게 해보세요!
-                      </div>
+                      <>
+                        <Img
+                          src={`${process.env.PUBLIC_URL}/resources/images/Comp_rule.png`}
+                          alt="character"
+                          width="40%"
+                          height="40%"
+                          style={{
+                            // position: "absolute",
+                            top: "0%", // Adjust as necessary
+                            left: "20%", // Centering the image if it's 80% width
+                          }}
+                          />
+                        <div style={{ textAlign: 'left' }}>
+                        <h1 style={{color:"red"}}>
+                        이 모드는 오징어 격투 입니다. 
+                        </h1>
+                        <br></br>
+                        <h4>
+                        1. 2 대 2 로 팀을 나누어 진행합니다. 
+                        </h4>
+                        <br></br>
+                        <h4>
+                        2. 정답을 맞추면 그리는 오징어와 맞히는 오징어의 역할이 바뀝니다. 
+                        </h4>
+                        <br></br>
+                        <h4>
+                        3. 각 라운드당 제한시간 70초 입니다.
+                        </h4>
+                        <br></br> 
+                        <h4>
+                        4.'방해하기' 아이템을 통해 상대팀을 최대한 헷갈리게 해보세요!
+                        </h4>
+                        </div>
+                      </>
                     )}
 
                     {selectedMode === "spy" && (
-                      <div>
-                      이 모드는 스파이 문어 입니다
-                      1. 4명의 플레이어가 함께 진행합니다.
-                      2. 4명 중 1명의 스파이 문어가 존재하며 나머지 3명은 평범한 오징어 입니다.
-                      3. 제시어 한 개가 주어지며, 스파이는 이 제시어를 알지 못 합니다.
-                      4. 카운트가 시작되면, 플레이어마다 10초씩 그림을 이어 그려서 제시어를 완성시킵니다.
-                      5. 모든 플레이어가 그림을 그리기 전까지, 스파이가 제시어를 눈치채 정답을 맞추면 스파이의 승리! 답을 맞출 기회는 총 3번입니다.
-                      6. 스파이가 답을 맞추지 못 한채로 모든 턴이 끝이 난다면, 플레이어들은 투표를 통해 스파이 문어를 색출합니다.
-                      7. 투표를 통해 색출한 플레이어가 스파이 문어이면 오징어들의 승리!
-                      문어를 색출하지 못하면 스파이 문어의 승리입니다.
+                      <>
+                        <Img
+                          src={`${process.env.PUBLIC_URL}/resources/images/Spy_rule.png`}
+                          alt="character"
+                          width="40%"
+                          height="40%"
+                          style={{
+                            // position: "absolute",
+                            top: "0%", // Adjust as necessary
+                            left: "20%", // Centering the image if it's 80% width
+                          }}
+                        />
+                      <div style={{ textAlign: 'left' }}>
+                        <h1 style={{color:"red"}}>이 모드는 스파이 문어 입니다 </h1>
+                        <br></br> <h4>1. 4명의 플레이어가 함께 진행합니다. </h4>
+                        <br></br> <h4>2. 4명 중 1명의 스파이 문어가 존재하며 나머지 3명은 평범한 오징어 입니다. </h4>
+                        <br></br> <h4>3. 제시어 한 개가 주어지며, 스파이는 이 제시어를 알지 못 합니다. </h4>
+                        <br></br> <h4>4. 카운트가 시작되면, 플레이어마다 10초씩 그림을 이어 그려서 제시어를 완성시킵니다.</h4>
+                        {/* <br></br> <h4>5. 모든 플레이어가 그림을 그리기 전까지, 스파이가 제시어를 눈치채 정답을 맞추면 스파이의 승리! 답을 맞출 기회는 총 3번입니다.</h4> 
+                        <br></br> <h4>6. 스파이가 답을 맞추지 못 한채로 모든 턴이 끝이 난다면, 플레이어들은 투표를 통해 스파이 문어를 색출합니다. </h4>
+                        <br></br> <h4>7. 투표를 통해 색출한 플레이어가 스파이 문어이면 오징어들의 승리! 문어를 색출하지 못하면 스파이 문어의 승리입니다.</h4> */}
                       </div>
+                      </>
                     )}
                   </Flex>
                 </Box>
